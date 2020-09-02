7b301a4d0406b8d936961b241024bba8c488cc24
KiBee <killerbee1205@gmail.com>
2020-09-03 03:54:14 +1200

    repair static folder

--- ilh/__init__.py
+++ ilh/__init__.py
@@ -1,11 +1,11 @@
+import os
+
 from flask import Flask
-
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
-app = Flask(__name__)
+# static_path = os.path.join(project_root, '../static')
+app = Flask(__name__, static_folder='../static')
 app.secret_key = 'gachimuchi'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://kibee:1234@localhost:3306/first_flask'
